db5f41f5206faca297022c3e6babbb174ee2f89b
Ashwani Kumar Singh <134192201+2005Ashwani@users.noreply.github.com>
2025-12-06 18:55:28 +0530

    Uncomment CORS middleware and update allowed origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,30 +13,14 @@ const User = require("./models/user")
 
 
 
-// const cors = require("cors")
-// const paymentIntegration = require("./routes/paymentIntegration")
+const cors = require("cors")
+const paymentIntegration = require("./routes/paymentIntegration")
 
-// // Use its as a middleware    // Connect Frontend and Backend
-// app.use(cors({
-//     origin: 'http://localhost:5173', 
-    
-//     credentials: true, 
-// }));
-
-
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://leetcode-backend-2-frontend.onrender.com'
-];
+// Use its as a middleware    // Connect Frontend and Backend
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+    origin: 'http://localhost:5173', 
+    
+    credentials: true, 
 }));
 
 
